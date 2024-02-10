2ec61144770d3df65b63886e6552143b09b38b29
EduardoStarZ <pathver@gmail.com>
2024-02-10 17:06:07 -0300

    made the first sketches of how the system will work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::fs::File;
+use std::io::Write;
+use std::fs;
+
+mod funnel;
+
 fn main() {
-    println!("Hello, world!");
+    let filepaths : Vec<String> = fetch_files();
+
+    filepaths.iter().for_each(|path| println!("{path}"));
+}
+
+fn split_names(pathname: String) -> String {
+    pathname.replace("./build/", "")
+}
+
+fn fetch_files() -> Vec<String> {
+    let paths = fs::read_dir("./build/").unwrap();
+    let mut files : Vec<String> = Vec::new(); 
+    
+    for x in paths {
+        // println!("{}", x.unwrap().path().display());
+        if x.as_ref().unwrap().path().display().to_string().contains(".rs") {
+            files.push(x.unwrap().path().display().to_string());
+        }
+    }
+
+    files
+}
+
+fn modify() {
+    let mut funnel: File = File::create("funnel.rs").expect("couldn't create the file");
+
+    funnel.write("".as_bytes()).expect("couldn't write into the file");
 }
